bb69530c4840dcfb4344fbbafddea778e4d9aaf4
vishaljinode <vishaljinode@gmail.com>
2024-03-02 17:19:41 +0530

    add docs

---
+++ docs.js
@@ -0,0 +1,5 @@
+// Mongodb Username:vishaljinode
+// password:h1ipZsmAEOro8Ael
+// mongodb+srv://vishaljinode:Vishal@123@cluster0.s2ppxpl.mongodb.net/
+// heroku : vishaljinode@gmail.com
+// password :tamanna#143
